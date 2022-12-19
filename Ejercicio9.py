9e8b63a8dbe88fa5a9e1a4ac324ebba83946e650
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-19 09:45:03 +0100

    ejercio 9 mas

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,13 +1,18 @@
+import datetime
+
 class lunes:
     def __init__(self, año):
         self.año = año
 
-    def edad(self):
-        nacimiento = int(input("Ingrese su año de nacimiento: "))
-        return self.año - nacimiento
+    def edad(self, nacimiento):
+        self.nacimiento = int(input("Ingrese su año de nacimiento: "))
+        return self.año - self.nacimiento
 
     def trabajar(self):
         if self.edad() >= 22 and self.edad() <= 78:
             print("Puede trabajar")
+            lunes = (self.año - self.nacimiento)/7
         else:
             print("No puede trabajar")
+
+    
