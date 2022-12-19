7917d5fa8387e912e59b66c7e7cfb9f5d99cafde
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-19 09:19:16 +0100

    terminado 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -6,7 +6,7 @@ import sys
 
 def hollow_triangle(altura):
     for i in range(1, altura + 1):
-        print(("_" * (altura - i)) + "#" * i + "_" * (altura - i))
+        print(("_" * (altura - i)) + "#" * i + ("_" * (altura - i)))
 
 if __name__ == '__main__':
     altura = int(input("Introduce un numero: ").strip())
